e007089b9658535cfa35873d10337093228ea8f9
Mushu <92597474+MushuMartial@users.noreply.github.com>
2022-02-07 15:51:27 +0100

    Add files via upload

--- beta need for speed/main.c
+++ beta need for speed/main.c
@@ -6,7 +6,7 @@
 /*   By: tmartial <tmartial@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 10:24:27 by tmartial          #+#    #+#             */
-/*   Updated: 2022/02/07 14:58:24 by tmartial         ###   ########.fr       */
+/*   Updated: 2022/02/07 15:49:48 by tmartial         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int move_test(t_data *data)
 {
 	if (data->press == 1)
 	{
+		make_img(data);
 		make_player(data, 1);
 		data->x += data->add_x;
 		data->y += data->add_y;
@@ -35,9 +36,14 @@ int move_test(t_data *data)
 
 void moving(t_data *data, float x, float y, int pres)
 {
-	data->press = pres;
-	data->add_x += x;
-	data->add_y += y;
+	if (data->add_x != 0 && data->add_y != 0 && pres == 0)
+		(void)NULL;
+	else
+		data->press = pres;
+	if (data->add_x != x)
+		data->add_x += x;
+	if (data->add_y != y)
+		data->add_y += y;
 }
 
 int presskey(int keycode, t_data *data)
@@ -45,13 +51,13 @@ int presskey(int keycode, t_data *data)
 	if (keycode == 53)
 		exit_mlx(data);
 	if (keycode == 0)//left
-		moving(data, -0.01, 0, 1);
+		moving(data, -0.015, 0, 1);
 	else if (keycode == 2)//droit
-		moving(data, 0.01, 0, 1);
+		moving(data, 0.015, 0, 1);
 	else if (keycode == 13)//up
-		moving(data, 0, -0.01, 1);
+		moving(data, 0, -0.015, 1);
 	else if (keycode == 1)//down
-		moving(data, 0, 0.01, 1);
+		moving(data, 0, 0.015, 1);
 	return (0);
 }
 
@@ -60,13 +66,13 @@ int un_presskey(int keycode, t_data *data)
 	if (keycode == 53)
 		exit_mlx(data);
 	if (keycode == 0)//left
-		moving(data, +0.01, 0, 0);
+		moving(data, 0.015, 0, 0);
 	else if (keycode == 2)//droit
-		moving(data, -0.01, 0, 0);
+		moving(data, -0.015, 0, 0);
 	else if (keycode == 13)//up
-		moving(data, 0, +0.01, 0);
+		moving(data, 0, 0.015, 0);
 	else if (keycode == 1)//down
-		moving(data, 0, -0.01, 0);
+		moving(data, 0, -0.015, 0);
 	return (0);
 }
 
@@ -82,17 +88,8 @@ int main()
 	make_img(&data);
 	make_player(&data, 0);
 	mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
-	mlx_hook(data.win, 2, 1L << 0, presskey, &data); //b
-    mlx_hook(data.win, 3, 1L << 0, un_presskey, &data); //b
+	mlx_hook(data.win, 2, 1L << 0, presskey, &data);
+    mlx_hook(data.win, 3, 1L << 0, un_presskey, &data); 
 	mlx_loop_hook(data.mlx, &move_test, &data);
-	//mlx_key_hook(data.win, key_hook, &data);
-	//mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
-	//mlx_hook(data.mlx, 2, 1L << 0, &presskey, &param); //b
-    //mlx_hook(data.mlx, 3, 1L << 0, &unpresskey, &param); //b
-    //mlx_hook(, 17, 1L << 17, &close, &param); //b
-	//mlx_hook(data.win, 17, 0L, &exit_mlx, &data);
-	//mlx_loop_hook(data.mlx, &presskey, &data);
-	//mlx_hook(data.win, 2, 0L, &presskey, &data);
-	//mlx_hook(data.win, 3, 1L << 1, &un_presskey, &data);
 	mlx_loop(data.mlx);
 }
